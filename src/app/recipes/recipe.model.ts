ab05e60c3f1f78bc91aea899f7db53d2a644f0ba
Nada Mahmoud <nada.elroo799@gmail.com>
2022-06-27 16:27:15 +0200

    add bootstrap v3

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,3 @@
 export class Recipe{
-    public Name: string;
-    public Description: string;
-    public ImagePath: string;
-    
-    constructor(name:string, description:string, imageUrl:string)
-    {
-        this.Name = name;
-        this.Description = description;
-        this.ImagePath = imageUrl;    
-    }
+    constructor(public name:string, public description:string, public imagePath:string){}
 }
